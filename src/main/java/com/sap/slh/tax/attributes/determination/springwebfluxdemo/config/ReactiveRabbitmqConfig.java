0288a0bb8ba17bc797c0ac58b58aabdf5414c1f2
I347159 <I347159@INLN56593992A.apj.global.corp.sap>
2020-03-31 03:17:59 +0530

    commit changes with respect to rabbitmq

--- src/main/java/com/sap/slh/tax/attributes/determination/springwebfluxdemo/config/ReactiveRabbitmqConfig.java
+++ src/main/java/com/sap/slh/tax/attributes/determination/springwebfluxdemo/config/ReactiveRabbitmqConfig.java
@@ -2,6 +2,7 @@ package com.sap.slh.tax.attributes.determination.springwebfluxdemo.config;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
@@ -11,11 +12,12 @@ import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.AsyncRabbitTemplate;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +26,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableRabbit
 @Configuration
 public class ReactiveRabbitmqConfig {
-	
+
 	@Autowired
 	private AmqpAdmin amqpAdmin;
 
@@ -36,6 +38,9 @@ public class ReactiveRabbitmqConfig {
 
 	@Value("${rabbitmq.txs.routingKey}")
 	public String routingKey;
+	
+	@Autowired
+	private CachingConnectionFactory cachingConnectionFactory;
 
 	@PostConstruct
 	public void initializeQueue() {
@@ -48,20 +53,26 @@ public class ReactiveRabbitmqConfig {
 		amqpAdmin.declareQueue(taxAttributesDeterminationQueue);
 		amqpAdmin.declareBinding(binding);
 	}
-	
-//	@Bean
-//	public ConnectionFactory connectionFactory() {
-//	return new CachingConnectionFactory();
-//	}
-//
-//	 @Bean
-//	    AsyncRabbitTemplate template() {
-//		 RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-//		 rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
-////		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory());
-////		container.setQueueNames("tax.webflux.reactor.queue1");
-//	        return new AsyncRabbitTemplate(rabbitTemplate);
-//	    }
 
+	@Bean
+	public MessageConverter converter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(cachingConnectionFactory);
+		factory.setMismatchedQueuesFatal(true);
+		factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+		factory.setMessageConverter(converter());
+		return factory;
+	}
+
+	
+	@Bean
+    public AsyncRabbitTemplate asyncTemplate(RabbitTemplate template) {
+        return new AsyncRabbitTemplate(template);
+    }
 
 }
--- src/main/java/com/sap/slh/tax/attributes/determination/springwebfluxdemo/consumer/TaxLinesDetermineConsumer.java
+++ src/main/java/com/sap/slh/tax/attributes/determination/springwebfluxdemo/consumer/TaxLinesDetermineConsumer.java
@@ -1,39 +1,40 @@
-package com.sap.slh.tax.attributes.determination.springwebfluxdemo.consumer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageProperties;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.sap.slh.tax.attributes.determination.springwebfluxdemo.model.TaxDetails;
-import com.sap.slh.tax.attributes.determination.springwebfluxdemo.model.TaxLine;
-import com.sap.slh.tax.attributes.determination.springwebfluxdemo.service.TaxLineService;
-import com.sap.slh.tax.attributes.determination.springwebfluxdemo.util.JsonUtil;
-
-import reactor.core.publisher.Flux;
-
-@Component
-public class TaxLinesDetermineConsumer {
-	
-	private static final Logger log = LoggerFactory.getLogger(TaxLinesDetermineConsumer.class);
-	
-	@Autowired
-	private TaxLineService taxLineService;
-	
-	@RabbitListener(queues = "${rabbitmq.txs.queue}", returnExceptions = "true")
-	public Flux<TaxLine> handleMessages(Message requestMessage)
-	{
-		TaxDetails request = JsonUtil.toObjectFromByte(requestMessage.getBody(),TaxDetails.class);
-		Flux<TaxLine> result = taxLineService.determineTaxLines(request);
-		log.error("received result from redis is{}",JsonUtil.toJsonString(result));
+//package com.sap.slh.tax.attributes.determination.springwebfluxdemo.consumer;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.amqp.core.Message;
+//import org.springframework.amqp.core.MessageProperties;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import com.sap.slh.tax.attributes.determination.springwebfluxdemo.model.TaxDetails;
+//import com.sap.slh.tax.attributes.determination.springwebfluxdemo.model.TaxLine;
+//import com.sap.slh.tax.attributes.determination.springwebfluxdemo.service.TaxLineService;
+//import com.sap.slh.tax.attributes.determination.springwebfluxdemo.util.JsonUtil;
+//
+//import reactor.core.publisher.Flux;
+//
+//@Component
+//public class TaxLinesDetermineConsumer {
+//	
+//	private static final Logger log = LoggerFactory.getLogger(TaxLinesDetermineConsumer.class);
+//	
+//	@Autowired
+//	private TaxLineService taxLineService;
+//	
+//	@RabbitListener(queues = "${rabbitmq.txs.queue}", returnExceptions = "true")
+//	public Message handleMessages(Message requestMessage)
+//	{
+//		TaxDetails request = JsonUtil.toObjectFromByte(requestMessage.getBody(),TaxDetails.class);
+//		Flux<TaxLine> result = taxLineService.determineTaxLines(request);
+//		log.error("received result from redis is{}",JsonUtil.toJsonString(result));
+//		
 //		MessageProperties messageProperties = new MessageProperties();
 //		Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
 //		Message sentMsg = converter.toMessage(result, messageProperties);
-		return result;	
-	}
-
-}
+//		return sentMsg;	
+//	}
+//
+//}
